fbe18b55ebbdff9bff04c3123723aa1cd5eacab5
Robert Taylor <robert.taylor426@gmail.com>
2020-02-19 10:49:22 +0000

    Updating to latest alpha version serverless offline.

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 module.exports.serverless = async event => {
     return {
         statusCode: 200,
+	header: {
+        	content-type: 'application/json'
+	},
         body: JSON.stringify(
             {
                 message: 'Go Serverless v1.0! Your function executed successfully!',
